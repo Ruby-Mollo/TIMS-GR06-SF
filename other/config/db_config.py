c57d1c27d0837e868ebeab61b97f1981f9843f00
AndresCT1 <142762428+AndresCT1@users.noreply.github.com>
2025-05-03 10:26:47 -0500

    Delete other directory

--- other/config/db_config.py
+++
@@ -1,6 +0,0 @@
-# config/db_config.py
-HOST = "localhost"
-USER = "root"
-PASSWORD = "root"
-DATABASE = "facturacion"
-
--- other/main.py
+++
@@ -1,5 +0,0 @@
-# main.py
-from views import main_window
-
-if __name__ == "__main__":
-    main_window.main()
